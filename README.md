3dce39714fa47cae7d109ff810ad6d46524623f0
Junpeii <123533057+Lehnz@users.noreply.github.com>
2023-02-21 17:55:50 +0000

    Inital commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Python-DiscordBot
-Discord.JS but it's actually Python.
+# Python Discord Bot Template
+## Discord.JS but it's Python instead.
+> I already added intents and made a message event with a `Ping Pong` command.
+Just add in your token in `client.run()` as a **string** if you're making this repo private. It's always best to put your token in a `config.json` or `.env` file when your repo public.
+
+> If you're using `config.json` make sure to add a `.gitignore` file and add the config file in there.
---
+++ index.py
@@ -0,0 +1,28 @@
+import discord
+import json
+
+## This is the config file, you can delete this if you want.
+
+with open('./config/config.json') as f:
+    config = json.load(f)
+    token = config['token']
+
+###########################################################
+
+class client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.lower().startswith('!ping'):
+            await message.reply('Pong!')
+
+
+intents = discord.Intents.all()
+intents.message_content = True
+
+client = client(intents=intents)
+client.run(token)
